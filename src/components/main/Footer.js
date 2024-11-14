87819af89b0501517108ab1f1f17be0d356fad3b
grace <hakyeong0@naver.com>
2024-11-14 12:59:23 +0900

    footerdesign

---
+++ src/components/main/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+    return (
+        <div className="Footer">
+            <img src={`${process.env.PUBLIC_URL}/img/logo_font.png`} alt="logo" />
+
+            <>
+                <p>서울특별시 강동구 천호대로 157길 14  (우)05335 email@email.com  대표전화 02-1234-1234<tr />
+                    대표이사 김두리, 개인정보보호책임자 김하나, 사업자등록번호 111-22-33333, 통신판매업신고번호 1111-서울강동-1111<tr />
+                    COPYRIGHT © MegaboxJoongAng, Inc. All rights reserved</p>
+            </>
+        </div>
+    )
+}
+
+
+export default Footer;
+
+// + 여유되면 페이지 맨 위로 올리는 top 버튼 추가 -> 플러팅?
