e73c3ecfa25d2a9aaebb06da9ea7b8cfce9695b1
hana <simhana0122@gmail.com>
2024-11-21 18:31:26 +0900

    내예약 초반만 완료

--- src/pages/Myreservation.js
+++ src/pages/Myreservation.js
@@ -1,8 +1,19 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import axiosInstance from "../axiosInstance"
 
 function Myreservation(){
+  const [reservationList, setReservationList] = useState([])
+  const [loading, setLoading] = useState(true);
 
-
+  useEffect(()=>{
+    axiosInstance.get("/doori/myreservations")
+      .then(response=>{
+        console.log(response.data)
+        setLoading(false)
+      }).catch(error=>{
+        console.log(error)
+      })
+  },[])
 
   return(
     <div>
