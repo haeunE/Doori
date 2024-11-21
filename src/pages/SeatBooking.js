1725b24522fa9cd481ef409d82a6da74fee3c4ff
hana <simhana0122@gmail.com>
2024-11-21 16:32:53 +0900

    seatbooking page는 구현 완료 + timetableId 보내주는 것만확인 되면 될듯 함

--- src/pages/SeatBooking.js
+++ src/pages/SeatBooking.js
@@ -2,19 +2,29 @@ import React, { useEffect, useState } from "react";
 import "./css/SeatBooking.css";
 import axiosInstance from "../axiosInstance";
 import SeatRow from "../components/js/SeatRow";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
 
 const SeatBooking = () => {
-  const [selectedSeats, setSelectedSeats] = useState([]);
-  const [price, setPrice] = useState("");
-  const [reservedSeats, setReservedSeats] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
- // timetable정보 받아올거임 -> reservationNB생성 해야됨 (가격=티켓*자리ea) -> reservaionNB보내줘야됨
- // -> reNB & seatNB이용해서 저장 필요함 어떻게
+// 앞에서 navigate로 정보 보내는 경우
+//   const clicked = () => {
+//   navigate("/newpage", { state: { value: {timetableId} });
+// };
 
- // 해결방법 - 함수 2개 만들어서 onclick했을때 할건데 어싱크 어웨잇으로 해야될거 같음
-  const reservedSeatsEX = ["A1", "A3", "B2"]; // 예약된 좌석 예시
+  const location = useLocation();
+  const timetableId = location.state.value; // 타임테이블 id
+
+  // const timetableId = 1; // timetable - 임시데이터
+
+  const [selectedSeats, setSelectedSeats] = useState([]); // 선택 좌석
+  const [reservedSeats, setReservedSeats] = useState([]); // 예약된 좌석
+  const [price, setPrice] = useState(""); // 가격
+  const [loading, setLoading] = useState(false); // 로딩중
+  const navigate = useNavigate(); // 페이지 이동
+ 
+  const data = {price : price, timetableId : timetableId, seatNb : selectedSeats}
+
+  // const reservedSeatsEX = ["A1", "A3", "B2"]; // 예약된 좌석 예시 - 임시데이터
 
   const seatLayout = [
     { row: "A", seats: [1, 2, 3, 4, 5] },
@@ -34,16 +44,21 @@ const SeatBooking = () => {
     });
   };
 
-  // useEffect(()=>{
-  //   axiosInstance.get('/doori/reservation/seats')
-  //     .then(response=>{
-  //       console.log(response.data)
-  //       setReservedSeats([...reservedSeats,...response.data])
-  //       setLoading(false)
-  //     }).catch(error=>{
-  //       console.log(error)
-  //     })
-  // }, [])
+  // 예약된 자석 정보들 가져와주는 것 필요
+  useEffect(()=>{
+    setLoading(true);
+    axiosInstance.get('/doori/reservation/seats', {
+      params : {timetableId : timetableId}
+    })
+      .then(response=>{
+        console.log(response.data)
+        setReservedSeats(response.data)
+        setLoading(false)
+      }).catch(error=>{
+        console.log(error)
+        setLoading(false)
+      })
+  }, [timetableId]);
 
   if(loading)
     return <div>로딩중</div> // 로딩중인 사진? 넣어도 됨
@@ -55,7 +70,7 @@ const SeatBooking = () => {
             key={row.row}
             rowNumber={row.row}
             seats={row.seats}
-            reservedSeats={reservedSeatsEX}
+            reservedSeats={reservedSeats}
             selectedSeats={selectedSeats}
             handleSeatClick={handleSeatClick}
           />
@@ -72,21 +87,26 @@ const SeatBooking = () => {
           <h4>가격 : {price}</h4>
         </div>
         <button className="seat-btn"
-        //  onClick={()=>{
-        //   const isConfirmed = window.confirm("예매하시겠습니까?")
-        //   if(isConfirmed){
-        //     axiosInstance.post("/doori/reservation")
-        //     .then(response=>{
-        //       console.log(response.data)
-        //     }).catch(error=>{
-        //       console.log(error)
-        //     })
-        //   }else{
-        //     alert("예매 취소 되었습니다.")
-        //     navigate("/doori")
-        //   }
-          
-        //  }}
+         onClick={()=>{
+          const isConfirmed = window.confirm("예매하시겠습니까?")
+          if(isConfirmed){
+            setLoading(true);
+            console.log(data)// 확인용
+            axiosInstance.post("/doori/booking", data)
+            .then(response=>{
+              console.log(response.data) // 확인용
+              alert("예매가 완료되었습니다.");
+              navigate("/doori") // 어디로 이동 시킬지 
+            }).catch(error=>{
+              console.log(error)
+              alert("예매에 실패했습니다. 다시 시도해 주세요");
+            })
+            .finally(()=> setLoading(false));
+          }else{
+            alert("예매 취소 되었습니다.")
+            navigate("/doori")
+          }
+         }}
         >
           선택완료
         </button>
