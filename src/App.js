ab8981c21d1eb08c433776c07d8c7fd8fc7c7538
grace <hakyeong0@naver.com>
2024-11-22 13:06:29 +0900

    myreservation 작업중

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ import Test from './utils/Test';
 import SeatBooking from './pages/SeatBooking';
 import MyReviews from './pages/MyReviews';
 import Myreservation from './pages/Myreservation';
-import WatchedMovie from './pages/WatchedMovie';
 
 function App() {
   const [isAuth, setIsAuth] = useState(false); // 유저 로그인 상태
@@ -41,9 +40,6 @@ function App() {
         <Route path='/doori/userupdate' element={<UserUpdate setIsAuth={setIsAuth}/>}/>
         <Route path='/doori/userverify' element={<UserVerify setIsAuth={setIsAuth}/>}/>
         <Route path='/doori/myreviews' element={<MyReviews setIsAuth={setIsAuth}/>}/>
-        <Route path='/doori/watchedmovie' element={<WatchedMovie setIsAuth={setIsAuth}/>}/>
-
-
 
         {/* 영화 정보에 대한 route */}
         <Route path='/doori/movies' element={<Movies setIsAuth={setIsAuth}/>}/>
--- src/pages/Myreservation.js
+++ src/pages/Myreservation.js
@@ -1,14 +1,45 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import axiosInstance from "../axiosInstance"
 
 function Myreservation(){
+  const [reservationList, setReservationList] = useState([])
+  const [loading, setLoading] = useState(true);
 
+  useEffect(()=>{
+    axiosInstance.get("/doori/myreservation")
+      .then(response=>{
+        console.log(response.data) // 확인
+        setReservationList([...reservationList,...response.data])
+        console.log(reservationList) // 확인
+        setLoading(false)
+      }).catch(error=>{
+        console.log(error)
+      })
+  },[])
 
+  if(loading)
+    return <div>로딩중</div>
 
-  return(
+  return (
     <div>
+      <div className="movieCard">
 
+        <img
+          className="poster"
+          src={link} // 포스터 url
+          alt="Movie Poster"
+        />
+      </div>
+
+      <div className="movieInfo">
+        <p className="title">제목: {title}</p>
+        <p className="date-time">상영일: {date}</p>
+        <p className="reservedSeat">좌석 번호: {reservedSeat}</p>
+        <p className="price">결제 금액: {price} 원</p>
+      </div>
     </div>
-  )
+  );
 }
+
 
 export default Myreservation
